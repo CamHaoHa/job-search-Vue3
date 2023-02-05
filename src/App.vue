2be3bfa5586d20700ffd0b46b16ded8a76aa6445
CamHaoHa <camhaovn@gmail.com>
2023-02-05 22:01:16 +1100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Oh Yeah" />
+    </div>
+  </header>
+
+  <main></main>
+</template>
+
+<style scoped></style>
