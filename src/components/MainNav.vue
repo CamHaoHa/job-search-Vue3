f22abd5a9f34569a5da9d3077b115685cfc566e5
CamHaoHa <camhaovn@gmail.com>
2023-02-08 00:28:28 +1100

    setting vitest global

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,9 +1,24 @@
 <script>
 export default {
   name: "MainNav",
+  data() {
+    return {
+      company: "Job Search v.1",
+      author: "Cam Hao HA",
+    };
+  },
 };
 </script>
 
 <template>
-  <header class="w-full text-sm">Main Nav</header>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 h-16 w-full bg-white">
+      <div
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+      >
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <h2 class="ml-8 flex h-full items-center">Developed By {{ author }}</h2>
+      </div>
+    </div>
+  </header>
 </template>
