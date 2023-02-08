9db1cbb95fde0f97358c1b90126b172d5d4f8847
CamHaoHa <camhaovn@gmail.com>
2023-02-08 20:06:37 +1100

    add mainNav and mainNav unit test

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,19 +3,44 @@ export default {
   name: "MainNav",
   data() {
     return {
-      company: "Job Search v.1",
+      company: "Vue.com",
+      url: "https://google.com",
+      menuItems: [
+        "Teams",
+        "Location",
+        "About Us",
+        "Career",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
 </script>
 
 <template>
-  <header class="w-full text-sm">
+  <header class="w-full text-xl font-bold">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-5xl">{{
+          company
+        }}</a>
+
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="ml-9 h-full first:ml-0"
+            >
+              <a href="" class="flex h-full items-center py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -4,6 +4,22 @@ import MainNav from "@/components/MainNav.vue";
 describe("MainNav", () => {
   it("display the logo", () => {
     render(MainNav);
-    screen.debug();
+    const companyName = screen.getByText("Vue.com");
+    expect(companyName).toBeInTheDocument();
+  });
+  it("display meny items for mainnav", () => {
+    render(MainNav);
+
+    const mainNavItems = screen.getAllByRole("listitem");
+    const mainNavTexts = mainNavItems.map((item) => item.textContent);
+    // console.log(mainNavTexts);
+    expect(mainNavTexts).toEqual([
+      "Teams",
+      "Location",
+      "About Us",
+      "Career",
+      "Students",
+      "Jobs",
+    ]);
   });
 });
