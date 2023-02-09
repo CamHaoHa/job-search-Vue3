ef489a08ce52c797691bb0010496f14532f5b021
CamHaoHa <camhaovn@gmail.com>
2023-02-09 16:04:28 +1100

    add test for ActionButton component

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/vue";
+import ActionButton from "@/components/ActionButton.vue";
+
+describe("ActionButton", () => {
+  it("render text", () => {
+    render(ActionButton, {
+      props: {
+        text: "sign in",
+        type: "primary",
+      },
+    });
+    const button = screen.getByRole("button", {
+      name: /sign in/i,
+    });
+    expect(button).toBeInTheDocument();
+  });
+
+  it("apply at least one css style", () => {
+    render(ActionButton, {
+      props: {
+        text: "sign in",
+        type: "primary",
+      },
+    });
+    const button = screen.getByRole("button", {
+      name: /sign in/i,
+    });
+    expect(button).toHaveClass("primary");
+  });
+});
