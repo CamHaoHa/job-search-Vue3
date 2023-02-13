172b5c615df9160d42c464217533c2bc442591fd
CamHaoHa <camhaovn@gmail.com>
2023-02-13 22:23:28 +1100

    test: add more test

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -5,7 +5,7 @@ import { RouterLinkStub } from "@vue/test-utils";
 describe("JobListing", () => {
   const createJobProps = (jobprops = {}) => ({
     title: "Vue Developer",
-    organisation: " Microsoft Corporation",
+    organization: " Microsoft Corporation",
     ...jobprops,
   });
 
@@ -28,5 +28,27 @@ describe("JobListing", () => {
     const jobTitle = createJobProps({ title: "Junior Developer" });
     renderJobListing(jobTitle);
     expect(screen.getByText("Junior Developer")).toBeInTheDocument();
+  });
+
+  it("render the job organisation", () => {
+    const jobOrganisation = createJobProps({ organization: "Apple" });
+    renderJobListing(jobOrganisation);
+    expect(screen.getByText("Apple")).toBeInTheDocument();
+  });
+
+  it("render the job locations", () => {
+    const jobLocations = createJobProps({ locations: ["Sydney", "Melbourne"] });
+    renderJobListing(jobLocations);
+    expect(screen.getByText("Sydney")).toBeInTheDocument();
+    expect(screen.getByText("Melbourne")).toBeInTheDocument();
+  });
+
+  it("render the job requirement", () => {
+    const jobRequirement = createJobProps({
+      minimumQualifications: ["Vue foundation", "JS foundation"],
+    });
+    renderJobListing(jobRequirement);
+    expect(screen.getByText("Vue foundation")).toBeInTheDocument();
+    expect(screen.getByText("JS foundation")).toBeInTheDocument();
   });
 });
