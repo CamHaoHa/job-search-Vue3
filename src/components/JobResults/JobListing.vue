81571f6f50d5d1f1b49f09bb9a18d4cc2fb90864
CamHaoHa <camhaovn@gmail.com>
2023-02-13 17:57:00 +1100

    feat: add mock jobs data and render them to jobListings view

---
+++ src/components/JobResults/JobListing.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+  name: "JobListing",
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    jobPageLink() {
+      return `jobs/results/${this.job.id}`;
+    },
+  },
+};
+</script>
+
+<template>
+  <li class="mb-7">
+    <router-link
+      :to="jobPageLink"
+      class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
+    >
+      <div class="border-brand-gray- mx-8 border-b border-solid pt-5 pb-2">
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
+        <div class="flex flex-row align-middle">
+          <div class="mr-5">
+            <span>{{ job.organization }}</span>
+          </div>
+
+          <div>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="mr-5 inline-block"
+              >
+                {{ location }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="px-8 py-4">
+        <div>
+          <h3 class="mt-1 mb-2">Requirement:</h3>
+          <div>
+            <ul class="list-disc pl-8">
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="mt-2 text-center">
+          <router-link :to="jobPageLink" class="text-brand-blue-1">
+            Expand
+          </router-link>
+        </div>
+      </div>
+    </router-link>
+  </li>
+</template>
--- src/components/JobResults/JobListtings.vue
+++ src/components/JobResults/JobListtings.vue
@@ -1,8 +1,25 @@
 <script>
+import JobListing from "@/components/JobResults/JobListing.vue";
+import axios from "axios";
 export default {
   name: "JobListings",
+  components: { JobListing },
+
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  async mounted() {
+    const response = await axios.get("http://localhost:3000/jobs");
+    this.jobs = response.data;
+  },
 };
 </script>
 <template>
-  <main class="flex-auto bg-brand-gray-2 p-8">Job Listings Component</main>
+  <main class="flex-auto bg-brand-gray-2 p-8">
+    <ol>
+      <job-listing v-for="job in jobs" :key="job.id" :job="job" />
+    </ol>
+  </main>
 </template>
