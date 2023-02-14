733a81a0c1e79271f67418a16435858b30d4b6eb
CamHaoHa <camhaovn@gmail.com>
2023-02-14 21:14:27 +1100

    test: refactor file with factory function

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -6,23 +6,40 @@ import { RouterLinkStub } from "@vue/test-utils";
 vi.mock("axios");
 
 describe("JobListings", () => {
-  it("fetches the job data", () => {
-    axios.get.mockResolvedValue({ data: [] });
-    render(JobListings);
-    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: "5",
+      ...queryParams,
+    },
   });
 
-  it("display maximum of 10 jobs", async () => {
-    axios.get.mockResolvedValue({ data: Array(10).fill({}) });
+  const renderJobListings = ($route) => {
     render(JobListings, {
       global: {
+        mocks: {
+          $route: $route,
+        },
         stubs: {
           "router-link": RouterLinkStub,
         },
       },
     });
+  };
+
+  it("fetches the job data", () => {
+    axios.get.mockResolvedValue({ data: [] });
+    const $route = createRoute();
+    renderJobListings($route);
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  });
+
+  it("display maximum of 10 jobs", async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const queryParams = { page: "1" };
+    const $route = createRoute(queryParams);
+    renderJobListings($route);
     const jobListings = await screen.findAllByRole("listitem");
-    // expect(jobListings).toHaveLength(20);
-    expect(jobListings.length).toBe(10);
+    expect(jobListings).toHaveLength(10);
+    // expect(jobListings.length).toBe(10);
   });
 });
