c032ce5a4423e53f977db802a6a4243930b74c1b
CamHaoHa <camhaovn@gmail.com>
2023-02-16 20:04:55 +1100

    build : install pinia and bootstrap pinia to main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { createPinia } from "pinia";
 import router from "@/router";
 
 import App from "./App.vue";
@@ -9,7 +10,9 @@ import "@/index.css";
 
 library.add(faSearch);
 
+const pinia = createPinia();
 createApp(App)
+  .use(pinia)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
