e680dd88323cbc97b8adca2b01aa5866925a4779
CamHaoHa <hao@pedalverse.io>
2023-02-20 22:39:47 +1100

    test: refactor test to improve readable

--- tests/unit/components/Share/CollapsibleAccordion.test.js
+++ tests/unit/components/Share/CollapsibleAccordion.test.js
@@ -4,7 +4,7 @@ import userEvent from "@testing-library/user-event";
 import CollapsibleAccordion from "@/components/Share/CollapsibleAccordion.vue";
 
 describe("CollapsibleAccordion", () => {
-  it("render child content", async () => {
+  const renderCollapsibleAccordion = (config = {}) => {
     render(CollapsibleAccordion, {
       global: {
         stubs: {
@@ -17,7 +17,15 @@ describe("CollapsibleAccordion", () => {
       slots: {
         default: "<p>nested child</p>",
       },
+      ...config,
     });
+  };
+
+  it("render child content", async () => {
+    const props = { header: "category" };
+    const slots = { default: "<p>nested child</p>" };
+    const config = { props, slots };
+    renderCollapsibleAccordion(config);
     expect(screen.queryByText("nested child")).not.toBeInTheDocument();
     const button = screen.getByRole("button", { name: /category/i });
     await userEvent.click(button);
@@ -26,16 +34,10 @@ describe("CollapsibleAccordion", () => {
 
   describe("when parent does not custom child content", () => {
     it("render fallback slot content", async () => {
-      render(CollapsibleAccordion, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        props: {
-          header: "category",
-        },
-      });
+      const props = { header: "category" };
+      const slots = {};
+      const config = { props, slots };
+      renderCollapsibleAccordion(config);
       const button = screen.getByRole("button", { name: /category/i });
       await userEvent.click(button);
       expect(screen.getByText("Default Slot")).toBeInTheDocument();
