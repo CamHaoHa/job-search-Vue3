ae2a18d784c9437d85a325f2ed0236b44e77c83c
CamHaoHa <hao@pedalverse.io>
2023-02-21 22:43:08 +1100

    feat: using GET to take unique organization from database

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -2,6 +2,8 @@ import { defineStore } from "pinia";
 import getJobs from "@/api/getJobs";
 
 export const FETCH_JOBS = "FETCH_JOBS";
+export const UNIQUE_ORGANIZATION = "UNIQUE_ORGANIZATION";
+
 export const useJobsStore = defineStore("jobs", {
   state: () => ({
     jobs: [],
@@ -11,6 +13,14 @@ export const useJobsStore = defineStore("jobs", {
     async [FETCH_JOBS]() {
       const jobs = await getJobs();
       this.jobs = jobs;
+    },
+  },
+
+  getters: {
+    [UNIQUE_ORGANIZATION](state) {
+      const uniqueOrganization = new Set();
+      state.jobs.forEach((job) => uniqueOrganization.add(job.organization));
+      return uniqueOrganization;
     },
   },
 });
