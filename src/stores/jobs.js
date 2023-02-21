ab7c85f7d1e5dd6a7a4ab33b1854045d0ace41ef
CamHaoHa <hao@pedalverse.io>
2023-02-21 23:00:04 +1100

    test: update jobs store test, fixed typo in jobs store.vue

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import getJobs from "@/api/getJobs";
 
 export const FETCH_JOBS = "FETCH_JOBS";
-export const UNIQUE_ORGANIZATION = "UNIQUE_ORGANIZATION";
+export const UNIQUE_ORGANIZATIONS = "UNIQUE_ORGANIZATIONS";
 
 export const useJobsStore = defineStore("jobs", {
   state: () => ({
@@ -17,7 +17,7 @@ export const useJobsStore = defineStore("jobs", {
   },
 
   getters: {
-    [UNIQUE_ORGANIZATION](state) {
+    [UNIQUE_ORGANIZATIONS](state) {
       const uniqueOrganization = new Set();
       state.jobs.forEach((job) => uniqueOrganization.add(job.organization));
       return uniqueOrganization;
--- tests/unit/stores/jobs.test.js
+++ tests/unit/stores/jobs.test.js
@@ -9,7 +9,7 @@ describe("state", () => {
     setActivePinia(createPinia());
   });
 
-  it("stores job listings", () => {
+  it("stores empty array before fetch data", () => {
     const store = useJobsStore();
     expect(store.jobs).toEqual([]);
   });
@@ -19,12 +19,33 @@ describe("state", () => {
   beforeEach(() => {
     setActivePinia(createPinia());
   });
+
   describe("FETCH_JOBS", () => {
     it("makes API requests and store received data", async () => {
       axios.get.mockResolvedValue({ data: ["Job1", "Job2"] });
       const store = useJobsStore();
       await store.FETCH_JOBS();
       expect(store.jobs).toEqual(["Job1", "Job2"]);
+    });
+  });
+});
+
+describe("getter", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  describe("UNIQUE_ORGANIZATION", () => {
+    it("find unique organizations in the list of jobs", () => {
+      const store = useJobsStore();
+      store.jobs = [
+        { organization: "Meta" },
+        { organization: "Alphabet" },
+        { organization: "Meta" },
+      ];
+      const result = store.UNIQUE_ORGANIZATIONS;
+
+      expect(result).toEqual(new Set(["Alphabet", "Meta"]));
     });
   });
 });
