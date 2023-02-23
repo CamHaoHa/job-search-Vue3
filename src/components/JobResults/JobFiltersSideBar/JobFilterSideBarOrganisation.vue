7d55f95c5347283799e6f924f151b23b56e0d00b
CamHaoHa <hao@pedalverse.io>
2023-02-23 21:18:53 +1100

    refactor: make code more dynamic with mapstate and pinia

--- src/components/JobResults/JobFiltersSideBar/JobFilterSideBarOrganisation.vue
+++ src/components/JobResults/JobFiltersSideBar/JobFilterSideBarOrganisation.vue
@@ -1,9 +1,14 @@
 <script>
 import CollapsibleAccordion from "@/components/Share/CollapsibleAccordion.vue";
+import { mapState } from "pinia";
+import { useJobsStore, UNIQUE_ORGANIZATIONS } from "@/stores/jobs.js";
 
 export default {
   name: "JobFilterSideBarOrganisation",
   components: { CollapsibleAccordion },
+  computed: {
+    ...mapState(useJobsStore, [UNIQUE_ORGANIZATIONS]),
+  },
 };
 </script>
 
@@ -12,24 +17,13 @@ export default {
     <div class="mt-5">
       <fieldset>
         <ul class="flex flex-row flex-wrap">
-          <li class="h-8 w-1/2">
-            <input id="VueOz" type="checkbox" class="mr-3" />
-            <label for="VueOz">VueOz</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="Between Vue And Me" type="checkbox" class="mr-3" />
-            <label for="Between Vue and Me">Between Vue</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-            <label for="Et Vue Brute">Et Vue Brute</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="Vue and a Half men" type="checkbox" class="mr-3" />
-            <label for="Vue and a Half men">Vue and a Half men</label>
+          <li
+            v-for="organization in UNIQUE_ORGANIZATIONS"
+            :key="organization"
+            class="h-8 w-1/2"
+          >
+            <input :id="organization" type="checkbox" class="mr-3" />
+            <label for="organization">{{ organization }}</label>
           </li>
         </ul>
       </fieldset>
