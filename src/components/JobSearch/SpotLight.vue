9fa754fb9aca8b3f261d450df63b6534b378bc9a
CamHaoHa <camhaovn@gmail.com>
2023-03-08 21:33:54 +1100

    refactor spotlight file with onMounted helper function from vue composition

--- src/components/JobSearch/SpotLight.vue
+++ src/components/JobSearch/SpotLight.vue
@@ -1,19 +1,14 @@
-<script>
+<script setup>
 import axios from "axios";
-export default {
-  name: "SpotLight",
-  data() {
-    return {
-      spotlights: [],
-    };
-  },
-  async mounted() {
-    const baseUrl = import.meta.env.VITE_APP_API_URL;
-    const url = `${baseUrl}/spotlights`;
-    const response = await axios.get(url);
-    this.spotlights = response.data;
-  },
+import { ref, onMounted } from "vue";
+const spotlights = ref([]);
+const getSpotLights = async () => {
+  const baseUrl = import.meta.env.VITE_APP_API_URL;
+  const url = `${baseUrl}/spotlights`;
+  const response = await axios.get(url);
+  spotlights.value = response.data;
 };
+onMounted(getSpotLights);
 </script>
 
 <template>
