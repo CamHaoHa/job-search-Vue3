5f412f26813543b79a487b110944d85366a6c7e9
CamHaoHa <camhaovn@gmail.com>
2023-03-11 22:01:43 +1100

    feat: update getJobAPI call with typescript

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+//this syntax is used to reference a type declaration file for the vite development server's client - side APi
+interface ImportMetaEnv {
+  readonly VITE_APP_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
