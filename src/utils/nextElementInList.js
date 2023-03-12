361b5b8123d21644aee155b930e61c669f63c545
CamHaoHa <camhaovn@gmail.com>
2023-03-12 23:59:37 +1100

    refactor: update utils files to ts

--- src/utils/nextElementInList.js
+++
@@ -1,8 +0,0 @@
-const nextElemenInList = (list, value) => {
-  const currentValueIndex = list.indexOf(value);
-  const nextValueIndex = (currentValueIndex + 1) % list.length;
-  const nextValue = list[nextValueIndex];
-  return nextValue;
-};
-
-export default nextElemenInList;
---
+++ src/utils/nextElementInList.ts
@@ -0,0 +1,8 @@
+const nextElemenInList = <T>(list: T[], value: T) => {
+  const currentValueIndex = list.indexOf(value);
+  const nextValueIndex = (currentValueIndex + 1) % list.length;
+  const nextValue = list[nextValueIndex];
+  return nextValue;
+};
+
+export default nextElemenInList;
