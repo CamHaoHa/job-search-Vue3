4b72661d516a3206306608715e1483eead8148f1
CamHaoHa <camhaovn@gmail.com>
2023-03-13 19:25:54 +1100

    fix: jobview.vue computed

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,4 +1,9 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const currentJobId = computed(() => route.params.id);
+</script>
 <template>
   <div>Job Page for job {{ currentJobId }}</div>
 </template>
